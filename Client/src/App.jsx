e32fdfb2e02c9f833cb35a014925e3675350501e
Arham097 <arhamhasan70@gmail.com>
2024-10-03 02:51:16 +0500

    quiz backgroun added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,17 +2,22 @@ import React, { Suspense } from "react";
 import Three from "./components/Three";
 import { Canvas } from "@react-three/fiber";
 import { Html } from "@react-three/drei";
+import BackgroundQuiz from "./components/BackgroundQuiz";
 
 const App = () => {
   return (
     <div>
-      <Canvas id="cn" style={{ width: "100%", height: "100vh" }}>
+      {/* <Canvas id="cn" style={{ width: "100%", height: "100vh" }}>
         <Suspense fallback={null}>
           <Three />
         </Suspense>
-      </Canvas>
+      </Canvas> */}
+      <BackgroundQuiz />
     </div>
   );
 };
 
 export default App;
+
+const skyboxApi =
+  "https://skybox.blockadelabs.com/e/1be88f0476b368d70b885f68cb138a84";
