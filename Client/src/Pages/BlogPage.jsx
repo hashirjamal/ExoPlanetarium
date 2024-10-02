3858c68bb7cbfdc067bc49b594d3ae3cd9c70348
Muhammad Bilal <m.bilal0111@gmail.com>
2024-10-02 02:55:51 -0700

    creating the UI of the blog page

---
+++ Client/src/Pages/BlogPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from "../Components/HeroSection";
+import OverviewSection from "../Components/OverviewSection";
+import BlogsPost from "../Components/BlogsPost";
+
+function BlogPage() {
+  return (
+    <>
+      <HeroSection />
+      <OverviewSection />
+      <BlogsPost />
+    </>
+  );
+}
+
+export default BlogPage;
