b7c1a328f7e6776743e9d3f2363a087455da2911
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-10-05 13:27:57 +0500

    arham ke liye push kar raha hu

--- Client/src/Pages/Quiz/QuizPage.jsx
+++ Client/src/Pages/Quiz/QuizPage.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import styles from './QuizPage.module.css';
 import Questions from '../Quiz/components/Questions';
-import AfterQuiz from '../../components/AfterQuiz';
-import BackgroundQuiz from '../../components/BackgroundQuiz';
+// import AfterQuiz from '../../components/AfterQuiz';
+// import BackgroundQuiz from '../../components/BackgroundQuiz';
+import Three from '../../components/Three';
+import { Canvas } from '@react-three/fiber';
 
 const QuizPage = () => {
     const [counter, setCounter] = useState(0);
@@ -18,16 +20,20 @@ const QuizPage = () => {
 
     return (
       <>
-          {/* <div className={styles.background}> */}
-          <BackgroundQuiz>
+        <Canvas
+        id="cn"
+        style={{ width: "100%", height: "100vh" }} // Ensure canvas takes full width and height
+        camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 5] }}
+      >
+        <Suspense fallback={null}>
+          <Three />
+        </Suspense>
+      </Canvas>
           <div className={styles.background}> 
               <div className={styles.modalBox}>
                 <Questions counter={counter} setCounter={handleAnswerSelected} />
               </div>
             </div>
-          </BackgroundQuiz>
-          {/* </div> */}
-          {showAfterQuiz && <AfterQuiz />}
       </>
     );
 }
