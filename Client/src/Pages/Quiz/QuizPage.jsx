6e1d6eda1f42d3eb947a31dfa1a2e4d5eed69501
Arham097 <arhamhasan70@gmail.com>
2024-10-05 13:31:26 +0500

    updated

--- Client/src/Pages/Quiz/QuizPage.jsx
+++ Client/src/Pages/Quiz/QuizPage.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react';
-import styles from './QuizPage.module.css';
-import Questions from '../Quiz/components/Questions';
-import AfterQuiz from '../../components/AfterQuiz';
-import BackgroundQuiz from '../../components/BackgroundQuiz';
+import { useState } from "react";
+import styles from "./QuizPage.module.css";
+import Questions from "../Quiz/components/Questions";
+import AfterQuiz from "../../components/AfterQuiz";
+import BackgroundQuiz from "../../components/BackgroundQuiz";
 
 const QuizPage = () => {
-    const [counter, setCounter] = useState(0);
-    const [showAfterQuiz, setShowAfterQuiz] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const [showAfterQuiz, setShowAfterQuiz] = useState(false);
 
-    const handleAnswerSelected = () => {
-        setShowAfterQuiz(true);
-        setTimeout(() => {
-            setShowAfterQuiz(false);
-            setCounter(prev => prev + 1);
-        }, 19000);
-    };
+  const handleAnswerSelected = () => {
+    setShowAfterQuiz(true);
+    setTimeout(() => {
+      setShowAfterQuiz(false);
+      setCounter((prev) => prev + 1);
+    }, 19000);
+  };
 
-    return (
-      <>
-          {/* <div className={styles.background}> */}
-          <BackgroundQuiz>
-          <div className={styles.background}> 
-              <div className={styles.modalBox}>
-                <Questions counter={counter} setCounter={handleAnswerSelected} />
-              </div>
-            </div>
-          </BackgroundQuiz>
-          {/* </div> */}
-          {showAfterQuiz && <AfterQuiz />}
-      </>
-    );
-}
+  return (
+    <>
+      {/* <div className={styles.background}> */}
+      <BackgroundQuiz>
+        <div className={styles.background}>
+          <div className={styles.modalBox}>
+            <Questions counter={counter} setCounter={handleAnswerSelected} />
+          </div>
+        </div>
+      </BackgroundQuiz>
+      {/* </div> */}
+      {showAfterQuiz && <AfterQuiz />}
+    </>
+  );
+};
 
 export default QuizPage;
--- Client/src/components/BackgroundQuiz.jsx
+++ Client/src/components/BackgroundQuiz.jsx
@@ -1,4 +1,4 @@
-const BackgroundQuiz = ({children}) => {
+const BackgroundQuiz = ({ children }) => {
   return (
     <>
       <div class="wrapper">
@@ -6,7 +6,7 @@ const BackgroundQuiz = ({children}) => {
           src="https://skybox.blockadelabs.com/e/1be88f0476b368d70b885f68cb138a84"
           style={{ width: "100%", height: "100vh", position: "relative" }}
         ></iframe>
-          {children}
+        {children}
         <div class="hide-cursor"></div>
       </div>
       <div></div>
