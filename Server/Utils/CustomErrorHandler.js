c61998a422ccdfa1f2a813275b0d9e8993d32e75
Muneer <muneerhussain420420@gmail.com>
2024-10-05 12:20:34 +0500

    Hello

---
+++ Server/Utils/CustomErrorHandler.js
@@ -0,0 +1,13 @@
+// Error handler
+class CustomError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.message = message;
+      this.statusCode = statusCode;
+      this.status =
+        this.statusCode >= 400 && this.statusCode < 500 ? "fail" : "error";
+      this.isOperational = true;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  module.exports = CustomError;
