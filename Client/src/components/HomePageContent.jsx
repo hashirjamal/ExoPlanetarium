838451d6923dc56d3e4f5f3c2fdb4b0535eb3f92
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-10-06 11:43:45 +0500

    merged ibad code

--- Client/src/components/HomePageContent.jsx
+++ Client/src/components/HomePageContent.jsx
@@ -9,17 +9,19 @@ const HomePageContentComponent = () => {
   const navigate = useNavigate();
   useEffect(() => {
     AOS.init();
-  }, [])
+  }, []);
 
   return (
-    <div className="p-10 w-full bg-black">
+    <div className="p-4 md:p-10 w-full bg-black">
       {HomePageContent.map((item, index) => (
-        <div className="my-[-250px] h-[125vh] pl-24" key={index}>
+        <div
+          className="my-10 md:my-[-250px] h-auto md:h-[125vh] pl-4 md:pl-24  max-sm:my-[120px]"
+          key={index}
+        >
           <div
             data-aos="fade-up"
-            className="flex flex-row md:flex-row bg-slate-900 text-white p-10 rounded-2xl opacity-90 gap-40 w-[80vw] h-96 shadow-slate-50 drop-shadow-xl"
+            className="flex flex-col md:flex-row bg-slate-900 text-white p-4 md:p-10 rounded-2xl opacity-90 gap-4 md:gap-40 w-full md:w-[80vw] h-auto shadow-slate-50 drop-shadow-xl"
             style={{
-              display: "flex",
               flexDirection: index % 2 === 0 ? "row" : "row-reverse",
               alignItems: "center",
               margin: "20px 0",
@@ -28,20 +30,29 @@ const HomePageContentComponent = () => {
             <img
               src={item.image}
               alt={item.heading}
-              style={{ width: "200px", height: "200px", objectFit: "cover", transition: "transform 0.3s" }}
-              className="rounded-full border-black hover:transform hover:scale-110 cursor-pointer"
+              className="w-32 h-32 md:w-52 md:h-52 object-cover transition-transform rounded-full border-black hover:scale-110 cursor-pointer"
             />
             <div
+              className="flex flex-col h-auto gap-4 md:gap-8"
               style={{
                 marginLeft: index % 2 === 0 ? "20px" : "0",
                 marginRight: index % 2 !== 0 ? "20px" : "0",
               }}
-              className="flex flex-col h-40 gap-8"
             >
-              <h2 data-aos="zoom-in" className="font-bold text-4xl">{item.heading}</h2>
-              <p data-aos="zoom-in" className={`text-xl font-medium ${style.paraFont} tracking-wider`}>{item.content}</p>
+              <h2
+                data-aos="zoom-in"
+                className="font-bold text-xl md:text-4xl"
+              >
+                {item.heading}
+              </h2>
+              <p
+                data-aos="zoom-in"
+                className={`text-sm md:text-xl font-medium ${style.paraFont} tracking-wider`}
+              >
+                {item.content}
+              </p>
               <button
-                className="bg-black w-40 p-2 rounded-2xl"
+                className="bg-black w-28 md:w-40 p-2 rounded-2xl"
                 onClick={() => navigate(item.links)}
               >
                 {item.button}
