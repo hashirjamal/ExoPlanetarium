d3054d4a6c6cd62fa2b5874c859ee12cb16ea104
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-11-18 15:17:43 +0500

    Update chatbotController.js

--- Server/Controller/chatbotController.js
+++ Server/Controller/chatbotController.js
@@ -3,32 +3,37 @@ const llmHandler = require("../Utils/AiUtils/llmHandler");
 const { getVectors } = require("../Utils/AiUtils/pcQuery");
 
 exports.handlePrompt = asyncErrorHandler(async (req, res, next) => {
+    const { userInp } = req.body;
+    const { convHistory } = req.body;
 
-    const {userInp} = req.body;
-    const {convHistory} = req.body;
+    const standaloneQues = await llmHandler.createQuestion(
+        userInp,
+        convHistory
+    );
+    console.log("Standalone:", standaloneQues);
 
-    const standaloneQues = await llmHandler.createQuestion(userInp,convHistory);
-    console.log("Standalone:",standaloneQues);
-    
-    const similarVecs = await getVectors(standaloneQues)
-  
+    const similarVecs = await getVectors(standaloneQues);
 
-    console.log("Retrieved information: ",similarVecs)
+    console.log("Retrieved information: ", similarVecs);
 
-    let nearestMatches = similarVecs.map((v,i)=>{
+    let nearestMatches = similarVecs.map((v, i) => {
         return v.metadata.text;
-    })
+    });
 
     nearestMatches = nearestMatches.join("\n");
 
     let history = convHistory.join("\n");
 
-    const result = await llmHandler.getFinalAns(nearestMatches,history,userInp)
+    const result = await llmHandler.getFinalAns(
+        nearestMatches,
+        history,
+        userInp
+    );
 
-    console.log(result)
+    console.log(result);
     res.status(200).send({
-        status:"Success",
-        message:"Response Fetched Successfully",
-        data:result
-    })
-  });
+        status: "Success",
+        message: "Response Fetched Successfully",
+        data: result,
+    });
+});
